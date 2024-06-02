40f570aa9898b285b2e371604c5f749d2e53ae96
Reem Tarek <reem1tarek9@gmail.com>
2024-06-02 17:37:40 +0300

    Delete testclient/client directory

--- testclient/client/src/user.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const UserComponent = ({ CurrentUser }) => { // Renamed user to UserComponent and properly destructured CurrentUser
-    return (
-        <div>user</div>
-    );
-}
-
-export default UserComponent;
